c0e27c897700de7dbd71d9c44be6f9eac36eeb45
Sebastian RODRIGUEZ <sebastian.rodriguez@rmit.edu.au>
2020-08-17 21:17:56 +1000

    Add basic Workout model.

---
+++ src/main/java/app/models/Workout.java
@@ -0,0 +1,36 @@
+package app.models;
+
+public class Workout {
+    private Long id = null;
+    private String name = null;
+    private String description = null;
+
+    public Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
