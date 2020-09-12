7b229cd3773bfed00e4fcf0ce02c2d6937d05f9c
Yuqing Wu <s3808626@student.rmit.edu.au>
2020-09-12 12:09:33 +0800

    weight java
    Signed-off-by: Your Name <you@example.com>

---
+++ src/main/resources/db/migration/V10__insert_weights.sql
@@ -0,0 +1,5 @@
+INSERT into weights(weight,Wtime) values ('78','2020-07-11');
+INSERT into weights(weight,Wtime) values ('82','2020-07-31');
+INSERT into weights(weight,Wtime) values ('86','2020-08-11');
+INSERT into weights(weight,Wtime) values ('85','2020-08-31');
+INSERT into weights(weight,Wtime) values ('83','2020-09-11');
